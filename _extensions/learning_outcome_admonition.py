025d443120809e5e149fbbfbced1f10ccd9c3fbe
Antonia Statt <49729189+astatt@users.noreply.github.com>
2025-05-16 11:24:11 -0500

    try new note type for learning outcomes

---
+++ _extensions/learning_outcome_admonition.py
@@ -0,0 +1,13 @@
+from docutils.parsers.rst.directives.admonitions import Admonition
+
+
+class LearningOutcome(Admonition):
+    """Learning Outcome admonition."""
+
+    def run(self):
+        self.arguments[0] = f"Learning Outcomes: {self.arguments[0]}"
+        return super().run()
+
+
+def setup(app):
+    app.add_directive("learning-outcome", LearningOutcome)
